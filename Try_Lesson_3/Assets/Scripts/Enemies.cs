77b50bcb3bff4d6c33108bfd58ccb0f98792b79a
Jean <BlasterJhan@gmail.com>
2019-04-24 14:12:24 +0300

    Здесь задание для 3-его и 4-го урока, Player- для 3-его урока, Player Phisics для 4-го

---
+++ Try_Lesson_3/Assets/Scripts/Enemies.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemies : MonoBehaviour
+{
+    public float moveSpeed = 3f;
+    public int HP = 2;
+}
---
+++ Try_Lesson_3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 2f;
+    public Transform firepoint;
+    public Bullet bulletPrefab;
+
+    private void Update()
+    {
+        var xInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(new Vector3(xInput, 0, 0)*Time.deltaTime*moveSpeed,Space.World);
+
+        if (Input.GetButtonDown("Fire1")) Fire();
+    }
+
+    private void Fire()
+    {
+         Instantiate(bulletPrefab, firepoint.position, firepoint.rotation);
+
+    }
+}
